f505035324755395886adcc91ee9c0773290d7dd
rawandkurdy <rawandkurdy@192.168.1.106>
2018-03-15 12:53:51 +0300

    Started development of the UI using JavaFX Initial Login defined

---
+++ src/uiFX/LoginController.java
@@ -0,0 +1,70 @@
+
+package uiFX;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author rawandkurdy
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private Button login;
+    @FXML
+    private Button register;
+    @FXML
+    private Label statues;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //First we check somethings to define either to enable register button or not
+        priorchecks();
+        
+    }    
+
+    @FXML
+    private void login(ActionEvent event) {
+     //TODO
+    }
+
+    @FXML
+    private void register(ActionEvent event) {
+    }
+    
+    public boolean priorchecks(){
+    //First db Check
+    if(resources.requirements.isDBavailable()){
+        //now lets check if there is an Doctor in our db
+        if(resources.requirements.returnAllDoctor().isEmpty()){
+        statues.setText("No Doctor Registered");
+        login.setDisable(true);
+        return false;}
+        else{
+            return true;}
+    }
+    else {
+    statues.setText("DB is not available");
+    login.setDisable(true);
+    register.setDisable(true);
+    return false;}
+    
+    }
+    
+}
