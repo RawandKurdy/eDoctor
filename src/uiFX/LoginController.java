e37581309a9762742543cc7b74ac3076d8b417b6
Rawand Kurdy <rawand.3115014011@uhd.edu.iq>
2018-03-16 23:13:15 +0300

    Login UI refined Added checks and Alerts And Scene Switcher

--- src/uiFX/LoginController.java
+++ src/uiFX/LoginController.java
@@ -1,17 +1,28 @@
 
 package uiFX;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import resources.doctor;
+import resources.receptionist_credentials;
+import resources.requirements;
 
 /**
  * FXML Controller class
@@ -35,6 +46,12 @@ public class LoginController implements Initializable {
     private Color x4;
     @FXML
     private Font x3;
+    
+    //Doctors
+    ArrayList<doctor> Docslist;
+    
+    //Receptionists
+    ArrayList<receptionist_credentials> recpList;
 
     /**
      * Initializes the controller class.
@@ -42,14 +59,99 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //First we check somethings to define either to enable register button or not
-        priorchecks();
+        if(priorchecks()){
+        Docslist=requirements.returnAllDoctor();
+        recpList=requirements.returnAllReceptionist_credentials();
+        }
         
     }    
 
     @FXML
-    private void login(ActionEvent event) {
-     //TODO
-    }
+    private void login(ActionEvent event) throws IOException {
+     //if we have users in our system
+     boolean found=false;
+     boolean usernamedoesntexist=true;
+     doctor doc = null;
+     receptionist_credentials recep = null;
+     
+     if(Docslist.size()+recpList.size()>0){
+         String username=this.username.getText();
+         String password=requirements.MD5(this.password.getText());
+         //first we start with Doctors list
+         for (doctor object : Docslist) {
+             if(object.getUser_Name().equalsIgnoreCase(username) ){
+                 if(object.getPassword().equals(password)){
+             found=true;
+             usernamedoesntexist=false;
+             doc=object;
+             recep=null;
+             break;}
+                 else{
+                     usernamedoesntexist=false;
+                 Alert alert = new Alert(AlertType.ERROR);
+                       alert.setTitle("Password");
+                       alert.setHeaderText("Entered password is wrong");
+                    //   alert.setContentText("try again");
+                       alert.showAndWait();
+                 }
+             }
+            
+         }
+         //if didnt found it their we try the receptionist too
+         if(!found){
+           for (receptionist_credentials object : recpList) {
+             if(object.getUsername().equalsIgnoreCase(username) ){
+                 if(object.getPassword().equals(password)){
+             found=true;
+             usernamedoesntexist=false;
+             recep=object;
+             doc=null;
+             break;}
+                     else{
+                     usernamedoesntexist=false;
+                 Alert alert = new Alert(AlertType.ERROR);
+                       alert.setTitle("Password");
+                       alert.setHeaderText("Entered password is wrong");
+                    //   alert.setContentText("try again");
+                       alert.showAndWait();
+                 }
+             }
+            
+         }
+         }
+         
+         
+      if(found){
+      
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("options.fxml"));
+        Parent optionsUIparent = loader.load();
+        Scene optionsUI = new Scene(optionsUIparent);
+        
+        //access the controller and call a method
+        OptionsController controller = loader.getController();
+        controller.initLoggedUser(doc, recep);
+        
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(optionsUI);
+        window.show();
+      }
+     
+     
+     
+     if(usernamedoesntexist){
+     
+     Alert alert = new Alert(AlertType.ERROR);
+                       alert.setTitle("Username");
+                       alert.setHeaderText("Username Doesnt exist");
+                     alert.setContentText("Username is case-sensitive");
+                       alert.showAndWait();
+     }
+     }
+       
+     }
 
     @FXML
     private void register(ActionEvent event) {
