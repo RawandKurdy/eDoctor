51af7e04178bcc76de975ac52a38e5e0e0f902be
Rawand F. Hama <rawand.3115014011@uhd.edu.iq>
2018-05-16 11:48:15 +0300

    Added a confirmation alert

--- src/resources/alerts.java
+++ src/resources/alerts.java
@@ -1,7 +1,10 @@
 
 package resources;
 
+import java.util.Optional;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 /**
  *
@@ -20,5 +23,17 @@ alert.setHeaderText(msg);
 
 alert.showAndWait();
     }
+       
+       public static Optional<ButtonType> confirmationDialog(){
+           Alert alert = new Alert(AlertType.CONFIRMATION);
+           alert.setTitle("Confirmation Dialog");
+           alert.setHeaderText("Are you sure about deleting this record?");
+           alert.setContentText("This action cannot be undo'ed");
+
+           Optional<ButtonType> result = alert.showAndWait();
+           return result;
+
+       
+       }
     
 }
