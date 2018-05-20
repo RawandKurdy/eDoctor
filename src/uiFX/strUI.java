b942b9e0da75a69a9e28bc09f9c765e3b1b497fd
Rawand Kurdy <rawand.3115014011@uhd.edu.iq>
2018-05-20 17:36:39 +0300

    upped the ver. No.

--- src/uiFX/strUI.java
+++ src/uiFX/strUI.java
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
  */
 public class strUI extends Application {
     
-    static String appname="eDoctors v0.1 Alpha";
+    static String appname="eDoctors v0.2 Alpha";
      static String appabout="Developed By Rawand \n aka Rawand Kurdy \n with MaRS Group \n for University of Human Development"
              + "\n 4th Stage Project \n Copyright 2018";
 
